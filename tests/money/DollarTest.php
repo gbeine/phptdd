23a1edaf38ff68f6fe3930ded8e7d5b06d1446f9
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-09 17:44:38 +0200

    Test refactoring
    Remove duplicated tests for Money::equals

--- tests/money/DollarTest.php
+++ tests/money/DollarTest.php
@@ -10,12 +10,6 @@ class DollarTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals(Money::dollar(12), $four->times(3));
 	}
 
-	public function testEquals() {
-		$five = Money::dollar(5);
-		$this->assertTrue($five->equals(Money::dollar(5)));
-		$this->assertFalse($five->equals(Money::dollar(4)));
-	}
-
 	public function testFactory() {
 		$eight = Money::dollar(8);
 		$this->assertInstanceOf('money\Dollar', $eight);
--- tests/money/EuroTest.php
+++ tests/money/EuroTest.php
@@ -10,12 +10,6 @@ class EuroTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals(Money::euro(12), $four->times(3));
 	}
 
-	public function testEquals() {
-		$five = Money::euro(5);
-		$this->assertTrue($five->equals(Money::euro(5)));
-		$this->assertFalse($five->equals(Money::euro(4)));
-	}
-
 	public function testFactory() {
 		$eight = Money::euro(8);
 		$this->assertInstanceOf('money\Euro', $eight);
