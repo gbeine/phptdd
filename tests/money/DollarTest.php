0a17814490edf68780acaabd86e3351752cd46b1
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-09 16:47:44 +0200

    Test side effects
    Add test method verifying that multiplication has no side effects on
    Dollar objects

--- tests/money/DollarTest.php
+++ tests/money/DollarTest.php
@@ -10,4 +10,11 @@ class DollarTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals(10, $five->amount());
 	}
 
+	public function testSideEffects() {
+		$four = new Dollar(4);
+		$four->times(2);
+		$this->assertEquals(8, $four->amount());
+		$four->times(3);
+		$this->assertEquals(12, $four->amount());
+	}
 }
