ce896572daebb743394403a38bba5ac48b808937
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-09 17:15:22 +0200

    Test refactoring, remove duplications

--- tests/money/DollarTest.php
+++ tests/money/DollarTest.php
@@ -5,17 +5,9 @@ namespace money;
 class DollarTest extends \PHPUnit_Framework_TestCase {
 
 	public function testMultiplication() {
-		$five = new Dollar(5);
-		$ten = $five->times(2);
-		$this->assertEquals(new Dollar(10), $ten);
-	}
-
-	public function testSideEffects() {
 		$four = new Dollar(4);
-		$eight = $four->times(2);
-		$this->assertEquals(new Dollar(8), $eight);
-		$twelve = $four->times(3);
-		$this->assertEquals(new Dollar(12), $twelve);
+		$this->assertEquals(new Dollar(8), $four->times(2));
+		$this->assertEquals(new Dollar(12), $four->times(3));
 	}
 
 	public function testEquals() {
