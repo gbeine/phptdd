c8047ad7b4b5ac05f3a05ccdc9c621f543f869ee
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-09 17:42:16 +0200

    Test refactoring
    Use factory instead of constructor

--- tests/money/EuroTest.php
+++ tests/money/EuroTest.php
@@ -5,15 +5,15 @@ namespace money;
 class EuroTest extends \PHPUnit_Framework_TestCase {
 
 	public function testMultiplication() {
-		$four = new Euro(4);
-		$this->assertEquals(new Euro(8), $four->times(2));
-		$this->assertEquals(new Euro(12), $four->times(3));
+		$four = Money::euro(4);
+		$this->assertEquals(Money::euro(8), $four->times(2));
+		$this->assertEquals(Money::euro(12), $four->times(3));
 	}
 
 	public function testEquals() {
-		$five = new Euro(5);
-		$this->assertTrue($five->equals(new Euro(5)));
-		$this->assertFalse($five->equals(new Euro(4)));
+		$five = Money::euro(5);
+		$this->assertTrue($five->equals(Money::euro(5)));
+		$this->assertFalse($five->equals(Money::euro(4)));
 	}
 
 	public function testFactory() {
--- tests/money/MoneyTest.php
+++ tests/money/MoneyTest.php
@@ -9,11 +9,11 @@ class MoneyTest extends \PHPUnit_Framework_TestCase {
 		$this->assertTrue($five->equals(Money::dollar(5)));
 		$this->assertFalse($five->equals(Money::dollar(4)));
 
-		$four = new Euro(4);
-		$this->assertTrue($four->equals(new Euro(4)));
-		$this->assertFalse($four->equals(new Euro(6)));
+		$four = Money::euro(4);
+		$this->assertTrue($four->equals(Money::euro(4)));
+		$this->assertFalse($four->equals(Money::euro(6)));
 
-		$this->assertFalse($five->equals(new Euro(5)));
+		$this->assertFalse($five->equals(Money::euro(5)));
 	}
 
 }
