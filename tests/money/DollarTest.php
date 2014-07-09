bacbce56abd6f2e881bcec452bafcd7a4335ad46
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-09 16:36:45 +0200

    Test prepare refactoring
    Dollar::amount has to be private

--- tests/money/DollarTest.php
+++ tests/money/DollarTest.php
@@ -7,7 +7,7 @@ class DollarTest extends \PHPUnit_Framework_TestCase {
 	public function testMultiplication() {
 		$five = new Dollar(5);
 		$five->times(2);
-		$this->assertEquals(10, $five->amount);
+		$this->assertEquals(10, $five->amount());
 	}
 
 }
