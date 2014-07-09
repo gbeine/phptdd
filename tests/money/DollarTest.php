f0b8af6002e3cd09a22d32581f11b19e651e5ddf
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-09 16:13:52 +0200

    Test multiply
    Will not compile, class Dollar is missing

---
+++ tests/money/DollarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace money;
+
+class DollarTest extends \PHPUnit_Framework_TestCase {
+
+	public function testMultiplication() {
+		$five = new Dollar(5);
+		$five->times(2);
+		$this->assertEquals(10, $five->amount);
+	}
+
+}
