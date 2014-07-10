c2e18c295dff516359b555af36133655fc0a2775
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-10 21:17:04 +0200

    Step: test go green
    Bank reduces Money objects

--- application/money/Bank.php
+++ application/money/Bank.php
@@ -5,6 +5,9 @@ namespace money;
 class Bank {
 
 	function reduce(Expression $exp, $currency) {
+		if ($exp instanceof Money) {
+			return $exp;
+		}
 		if ($exp instanceof Sum) {
 			$amount = $exp->addend->amount() + $exp->augend->amount();
 			return new Money($amount, $currency);
