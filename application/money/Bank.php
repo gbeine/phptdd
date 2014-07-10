218baabd90fd0ace4b45ff96504cc9a8e41dd4df
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-10 21:14:42 +0200

    Step: test go green
    Implement reduce for Sum objects in Bank

--- application/money/Bank.php
+++ application/money/Bank.php
@@ -5,6 +5,10 @@ namespace money;
 class Bank {
 
 	function reduce(Expression $exp, $currency) {
+		if ($exp instanceof Sum) {
+			$amount = $exp->addend->amount() + $exp->augend->amount();
+			return new Money($amount, $currency);
+		}
 		return null;
 	}
 }
