03052e2dc83d812b51aa4ecd997d9af35ab2d6e4
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-10 21:32:29 +0200

    Step: reduce duplication
    Bank uses exchange rate table

--- application/money/Bank.php
+++ application/money/Bank.php
@@ -4,15 +4,17 @@ namespace money;
 
 class Bank {
 
+	private $rates = array();
+
 	function reduce(Expression $exp, $currency) {
 		return $exp->reduce($this, $currency);
 	}
 
 	function addRate($from, $to, $rate) {
-
+		$this->rates[$from.$to] = $rate;
 	}
 
 	function rate($from, $to) {
-		return $from === "EUR" && $to === "USD" ? 2 : 1;
+		return $this->rates[$from.$to];
 	}
 }
