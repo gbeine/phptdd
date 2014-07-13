7f8144d6adfd4ce221c4e48884b607e346f040d9
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 19:31:24 +0200

    Test web
    Send shutdown after all test in class have been executed

--- tests/web/WebTest.php
+++ tests/web/WebTest.php
@@ -8,13 +8,19 @@ class WebTest extends \PHPUnit_Extensions_SeleniumTestCase {
 	protected $screenshotPath = '/tmp/screenshots';
 	protected $screenshotUrl = 'file:///tmp/screenshots';
 
+	private static $driver = null;
+
     protected function setUp() {
         $this->setBrowser('*firefox');
         $this->setBrowserUrl('http://www.google.com/');
+        self::$driver = $this->drivers[0];
+        $this->assertNotNull(self::$driver);
     }
 
-    protected function tearDown() {
-    	$this->shutDownSeleniumServer();
+    public static function tearDownAfterClass() {
+    	if (null !== self::$driver) {
+	    	self::$driver->shutDownSeleniumServer();
+    	}
     }
 
     public function testTitle() {
