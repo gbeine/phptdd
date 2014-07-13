cc82dd39d6d9091bedd923f76d3a2bb6108d39c9
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 18:09:42 +0200

    Assert refactor
    Use PHPUnit's IsEqual constraint to delegate comparison.

--- tests/PHPUnit/Bank/AbstractTestCase.php
+++ tests/PHPUnit/Bank/AbstractTestCase.php
@@ -6,7 +6,10 @@ use money\Money;
 
 abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase {
 
-	public static function assertMoneyValue($expected, Money $actual) {
-		return $actual->amount() === $expected;
+	public static function assertMoneyValue($expected, Money $actual, $message = '') {
+		$constraint = new \PHPUnit_Framework_Constraint_IsEqual(
+			$expected, 0, 10, FALSE, FALSE
+		);
+		return self::assertThat($actual->amount(), $constraint, $message);
 	}
 }
