62cf6660491b5d83ff03a73ac6789c436fced1f1
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 19:29:51 +0200

    Test web
    PHing starts Selenium RC server, test case sends shutdown command

--- tests/web/WebTest.php
+++ tests/web/WebTest.php
@@ -13,6 +13,10 @@ class WebTest extends \PHPUnit_Extensions_SeleniumTestCase {
         $this->setBrowserUrl('http://www.google.com/');
     }
 
+    protected function tearDown() {
+    	$this->shutDownSeleniumServer();
+    }
+
     public function testTitle() {
         $this->open('http://www.google.com/');
         $this->assertTitle('Google Fail');
