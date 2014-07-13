8458279f631c270780cbab273334a7f4366a8ac4
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 19:19:13 +0200

    Test web
    Take screenshots on test failures

--- tests/web/WebTest.php
+++ tests/web/WebTest.php
@@ -4,6 +4,10 @@ namespace web;
 
 class WebTest extends \PHPUnit_Extensions_SeleniumTestCase {
 
+	protected $captureScreenshotOnFailure = TRUE;
+	protected $screenshotPath = '/tmp/screenshots';
+	protected $screenshotUrl = 'file:///tmp/screenshots';
+
     protected function setUp() {
         $this->setBrowser('*firefox');
         $this->setBrowserUrl('http://www.google.com/');
@@ -11,7 +15,7 @@ class WebTest extends \PHPUnit_Extensions_SeleniumTestCase {
 
     public function testTitle() {
         $this->open('http://www.google.com/');
-        $this->assertTitle('Google');
+        $this->assertTitle('Google Fail');
     }
 }
 
