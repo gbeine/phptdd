bb92bd2ded847361f11705cffe2c4e443c66ea2d
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 16:23:13 +0200

    Test refactoring
    Move Bank related test cases into own test class

---
+++ tests/money/BankTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace money;
+
+class BankTest extends \PHPUnit_Framework_TestCase {
+
+	public function testReduceMoney() {
+		$bank = new Bank();
+		$reduced = $bank->reduce(Money::dollar(5), "USD");
+		$this->assertEquals(Money::dollar(5), $reduced);
+	}
+
+	public function testReduceMoneyDifferentCurrencies() {
+		$bank = new Bank();
+		$bank->addRate("EUR", "USD", 2);
+		$reduced = $bank->reduce(Money::euro(10), "USD");
+		$this->assertEquals(Money::dollar(5), $reduced);
+	}
+
+	public function testMixedAddition() {
+		$five = Money::dollar(5);
+		$ten = Money::euro(10);
+		$bank = new Bank();
+		$bank->addRate("EUR", "USD", 2);
+		$result = $bank->reduce($five->plus($ten), "USD");
+		$this->assertEquals(Money::dollar(10), $result);
+	}
+}
--- tests/money/MoneyTest.php
+++ tests/money/MoneyTest.php
@@ -37,26 +37,4 @@ class MoneyTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($five, $sum->augend());
 		$this->assertEquals($five, $sum->addend());
 	}
-
-	public function testReduceMoney() {
-		$bank = new Bank();
-		$reduced = $bank->reduce(Money::dollar(5), "USD");
-		$this->assertEquals(Money::dollar(5), $reduced);
-	}
-
-	public function testReduceMoneyDifferentCurrencies() {
-		$bank = new Bank();
-		$bank->addRate("EUR", "USD", 2);
-		$reduced = $bank->reduce(Money::euro(10), "USD");
-		$this->assertEquals(Money::dollar(5), $reduced);
-	}
-
-	public function testMixedAddition() {
-		$five = Money::dollar(5);
-		$ten = Money::euro(10);
-		$bank = new Bank();
-		$bank->addRate("EUR", "USD", 2);
-		$result = $bank->reduce($five->plus($ten), "USD");
-		$this->assertEquals(Money::dollar(10), $result);
-	}
 }
