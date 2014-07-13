58bd8cfffb81b6dbcce6aafbd66beeb916414134
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 18:25:59 +0200

    Test refactor
    Use assertion for comparing Money objects in all test cases

--- tests/money/BankTest.php
+++ tests/money/BankTest.php
@@ -7,16 +7,14 @@ class BankTest extends \PHPUnit\Bank\AbstractTestCase {
 	public function testReduceMoney() {
 		$bank = new Bank();
 		$reduced = $bank->reduce(Money::dollar(5), "USD");
-		$this->assertMoneyValue(5, $reduced);
-		$this->assertMoneyCurrency('USD', $reduced);
+		$this->assertMoneyEquals(Money::dollar(5), $reduced);
 	}
 
 	public function testReduceMoneyDifferentCurrencies() {
 		$bank = new Bank();
 		$bank->addRate("EUR", "USD", 2);
 		$reduced = $bank->reduce(Money::euro(10), "USD");
-		$this->assertMoneyValue(5, $reduced);
-		$this->assertMoneyCurrency('USD', $reduced);
+		$this->assertMoneyEquals(Money::dollar(5), $reduced);
 	}
 
 	public function testMixedAddition() {
@@ -25,7 +23,6 @@ class BankTest extends \PHPUnit\Bank\AbstractTestCase {
 		$bank = new Bank();
 		$bank->addRate("EUR", "USD", 2);
 		$result = $bank->reduce($five->plus($ten), "USD");
-		$this->assertMoneyValue(10, $result);
-		$this->assertMoneyCurrency('USD', $result);
+		$this->assertMoneyEquals(Money::dollar(10), $result);
 	}
 }
