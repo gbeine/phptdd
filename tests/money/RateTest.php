b1ddd5462085a1493cf5e0a252dd48384ef6a4f4
Gerrit Beine <gerrit.beine@gmx.de>
2014-07-13 18:59:55 +0200

    Test persistence
    Add test class and data set for Rate objects

---
+++ tests/money/RateTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace money;
+
+class RateTest extends \PHPUnit\Persistence\AbstractTestCase {
+
+	private $em;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->em = $this->getEntityManager();
+	}
+
+	public function getDataSet() {
+		return $this->createFlatXMLDataSet(dirname(__FILE__).'/../_files/rates.xml');
+	}
+
+	function testRatePersistence() {
+		$this->assertEquals(1, $this->getConnection()->getRowCount('rates'));
+
+		$rate = new Rate("EUR", "USD", 2);
+		$this->em->persist($rate);
+		$this->assertEquals(2, $this->getConnection()->getRowCount('rates'));
+	}
+}
